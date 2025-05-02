9cb97a0c3f7eca69c400e20409dfa7e7f73959b7
tvenceslau <tiago.venceslau@pdmfc.com>
2025-05-02 22:36:35 +0100

    DECAF-125 decorator extensions

--- src/decorators.ts
+++ src/decorators.ts
@@ -1,7 +1,7 @@
 import { LogLevel } from "./constants";
 import { Logging } from "./logging";
 
-export function log(
+export function logAs(
   level: LogLevel = LogLevel.info,
   benchmark: boolean = false,
   verbosity = 0
@@ -18,34 +18,43 @@ export function log(
       method(`called with ${args}`, verbosity);
       const start = Date.now();
       let end: number;
-      const result: any = originalMethod.apply(this, args);
-      if (result instanceof Promise) {
-        return result.then((r) => {
-          if (benchmark) {
-            end = Date.now();
-            if (benchmark) method(`completed in ${end - start}ms`, verbosity);
-          }
-          return r;
-        });
-      }
-      if (benchmark) {
-        end = Date.now();
-        if (benchmark) method(`completed in ${end - start}ms`, verbosity);
+
+      function finish(res: any, error?: Error) {
+        if (benchmark) end = Date.now();
+        const msg = [`${error ? "failed" : "completed"}`];
+        if (benchmark) msg.push(` in ${end - start}ms`);
+        msg.push(`: ${error ? "Error" : "Result"}: ${error ? error : res}`);
+        method(msg.join(""), verbosity);
+        if (error) throw error;
+        return res;
       }
 
-      return result;
+      let result: any;
+      try {
+        result = originalMethod.apply(this, args);
+      } catch (e: unknown) {
+        return finish(undefined, e as Error);
+      }
+      if (result instanceof Promise) {
+        return result.then(finish).catch((e) => finish(undefined, e));
+      }
+      return finish(result);
     };
   };
 }
 
-export function debug(benchmark: boolean = false) {
-  return log(LogLevel.debug, benchmark);
+export function logAsDebug(benchmark: boolean = false) {
+  return logAs(LogLevel.debug, benchmark);
 }
 
-export function info(benchmark: boolean = false) {
-  return log(LogLevel.info, benchmark);
+export function logAsInfo(benchmark: boolean = false) {
+  return logAs(LogLevel.info, benchmark);
 }
 
-export function verbose(verbosity = 0, benchmark: boolean = false) {
-  return log(LogLevel.verbose, benchmark, verbosity);
+export function logAsVerbose(verbosity = 0, benchmark: boolean = false) {
+  return logAs(LogLevel.verbose, benchmark, verbosity);
+}
+
+export function logAsSilly(benchmark: boolean = false) {
+  return logAs(LogLevel.silly, benchmark);
 }
